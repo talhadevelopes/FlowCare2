afe3c16e51e844279e0f8d23dc959cba787f4c70
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-02 19:04:21 +0530

    Women png bug

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -176,7 +176,7 @@ function App() {
           and connect with a community that cares about your well-being.
         </p>
         <button class="header-button">Join Us!</button>
-        <img src="../images/women.png" alt="women" />
+        <img src="../images/women.png" alt="women" class="feature-image"/>
       </div>
     </main>
 

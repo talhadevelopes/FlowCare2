bd39c270653773aa1ce7cff9f588b8bffa8d32f9
Mtalha10k <trainedbot10k@gmail.com>
2024-12-04 00:27:14 +0530

    Solved Something

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,31 @@
 import { useState } from "react";
-import { createBrowserRouter, RouterProvider, Routes, Route } from 'react-router-dom';
-import { Landing } from './components/Landing';
-import { Forum } from './components/Forum';
-import { Blogs } from './components/Blogs';
-import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Landing } from "./components/Landing";
+import { Forum } from "./components/Forum";
+import { Blogs } from "./components/Blogs";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Landing />
-},
-{
-  path: "/forum",
-  element: <Forum />
-},
-{
-  path: "/blogs",
-  element: <Blogs />
-}
-])
+    element: <Landing />,
+  },
+  {
+    path: "/forum",
+    element: <Forum />,
+  },
+  {
+    path: "/blogs",
+    element: <Blogs />,
+  },
+]);
 
 function App() {
-
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;

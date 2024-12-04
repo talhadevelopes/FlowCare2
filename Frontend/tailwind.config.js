46a74fbdb100b003e5b07a1958a897a90aee261b
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-04 10:13:37 +0530

    UI Bug Fixes

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,29 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: 'jit', // Force Just-in-Time mode
-    content: [
-      "./*.{html,js}",
-      "./ecom/**/*.{html,js}",
-      "./blogs/**/*.{html,js}",
-      "./forum/**/*.{html,js}",
-      "./consultation/**/*.{html,js}",
-      "./tracker/**/*.{html,js}",
-      "./scripts/**/*.{html,js}",
-    ],
-    theme: {
-      extend: {},
-    },
-  
-    safelist: [
-      ...Array.from({ length: 101 }, (_, i) => `ml-${i}`),
-      ...Array.from({ length: 101 }, (_, i) => `mr-${i}`),
-      ...Array.from({ length: 101 }, (_, i) => `mt-${i}`),
-      ...Array.from({ length: 101 }, (_, i) => `mb-${i}`),
-      ...Array.from({ length: 101 }, (_, i) => `py-${i}`),
-      ...Array.from({ length: 101 }, (_, i) => `px-${i}`),
-      ...Array.from({ length: 101 }, (_, i) => `h-${i}`),
-      ...Array.from({ length: 101 }, (_, i) => `w-${i}`),
-    ],
-  
-    plugins: [],
-  };
+  mode: "jit", // Force Just-in-Time mode
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./*.{html,js}",
+    "./ecom/**/*.{html,js}",
+    "./blogs/**/*.{html,js}",
+    "./forum/**/*.{html,js}",
+    "./consultation/**/*.{html,js}",
+    "./tracker/**/*.{html,js}",
+    "./scripts/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+
+  safelist: [
+    ...Array.from({ length: 101 }, (_, i) => `ml-${i}`),
+    ...Array.from({ length: 101 }, (_, i) => `mr-${i}`),
+    ...Array.from({ length: 101 }, (_, i) => `mt-${i}`),
+    ...Array.from({ length: 101 }, (_, i) => `mb-${i}`),
+    ...Array.from({ length: 101 }, (_, i) => `py-${i}`),
+    ...Array.from({ length: 101 }, (_, i) => `px-${i}`),
+    ...Array.from({ length: 101 }, (_, i) => `h-${i}`),
+    ...Array.from({ length: 101 }, (_, i) => `w-${i}`),
+  ],
+
+  plugins: [],
+};

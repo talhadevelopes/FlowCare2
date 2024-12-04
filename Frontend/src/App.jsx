c4c0c64b76c59dd6e1f49a4625877d64f3e105f1
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-04 09:50:16 +0530

    Bug Fixes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,10 @@
 import { useState } from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Routes, Route } from "react-router-dom";
 import { Landing } from "./components/Landing";
 import { Forum } from "./components/Forum";
 import { Blogs } from "./components/Blogs";
 import { Consultations } from "./components/Consultations";
-import "./App.css";
+
 import { Tracker } from "./components/Tracker";
 import { Ecom } from "./components/Ecom";
 

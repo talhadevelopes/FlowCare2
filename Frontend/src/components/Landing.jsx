af59cb7f9898c0876065d28c1c24b3e5ec1b6594
Mohammed Talha Ahmed <155841871+talhadevelopes@users.noreply.github.com>
2024-12-06 00:52:15 +0530

    Updated Landing.jsx

--- Frontend/src/components/Landing.jsx
+++ Frontend/src/components/Landing.jsx
@@ -181,7 +181,7 @@ export function Landing() {
                   </svg>
 
                   <span class="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible">
-                    Logout
+                    Sign-Up
                   </span>
                 </button>
               </form>

cd5bde17dca629a6c341094ffd386a15589daf89
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-05 20:49:57 +0530

    Login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Landing } from "./components/Landing";
 import { Forum } from "./components/Forum";
 import { Blogs } from "./components/Blogs";
 import { Consultations } from "./components/Consultations";
-
+import {Login } from "./components/Login";
 import { Tracker } from "./components/Tracker";
 import { Ecom } from "./components/Ecom";
 
@@ -33,6 +33,10 @@ const router = createBrowserRouter([
     path: "/Ecom",
     element: <Ecom />,
   },
+  {
+    path: "/Login",
+    element: <Login />,
+  }
 ]);
 
 function App() {
---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleEmail(e) {
+    setEmail(e.target.value);
+    console.log(email);
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value);
+    console.log(password);
+  }
+
+  function handleLogin(e) {
+    e.preventDefault(); // Prevent page reload
+    alert(`Email: ${email}`);
+    alert(`Password:, ${password}`);
+  }
+
+  return (
+    <div>
+      <div className="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form className="space-y-6" onSubmit={handleLogin}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email address
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required=""
+                    className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter your email address"
+                    onChange={handleEmail}
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required=""
+                    className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter your password"
+                    onChange={handlePassword}
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
